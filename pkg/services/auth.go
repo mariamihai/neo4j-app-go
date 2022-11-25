9ccfb4064934a393c2abc3a0ca43bcb94c835aa2
maria <maria.ic.mihai@gmail.com>
2022-11-25 13:21:44 +0100

    Save new user go test -v -run TestRegisterUser ./pkg/challenges

--- pkg/services/auth.go
+++ pkg/services/auth.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"fmt"
+	"github.com/neo4j-graphacademy/neoflix/pkg/ioutils"
 
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/neo4j-graphacademy/neoflix/pkg/fixtures"
@@ -44,15 +45,46 @@ func NewAuthService(loader *fixtures.FixtureLoader, driver neo4j.Driver, jwtSecr
 // with the returned user.
 // tag::register[]
 func (as *neo4jAuthService) Save(email, plainPassword, name string) (_ User, err error) {
-	// TODO: Handle Unique constraints in the database
-	if email != "graphacademy@neo4j.com" {
-		return nil, fmt.Errorf("An account already exists with this email address")
-	}
+	session := as.driver.NewSession(neo4j.SessionConfig{})
 
-	user, err := as.loader.ReadObject("fixtures/user.json")
+	defer func() {
+		err = ioutils.DeferredClose(session, err)
+	}()
+
+	result, err := session.WriteTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+		encryptedPassword, err := encryptPassword(plainPassword, 10)
+		if err != nil {
+			return nil, err
+		}
+
+		result, err := tx.Run(`
+			CREATE (u:User {
+				userId: randomUuid(),
+				email: $email,
+				password: $encrypted,
+				name: $name
+			})
+			RETURN u { .userId, .name, .email } as u`,
+			map[string]interface{}{
+				"email":     email,
+				"encrypted": encryptedPassword,
+				"name":      name,
+			})
+
+		record, err := result.Single()
+		if err != nil {
+			return nil, err
+		}
+
+		user, _ := record.Get("u")
+		return user, nil
+	})
+
 	if err != nil {
 		return nil, err
 	}
+
+	user := result.(map[string]interface{})
 
 	subject := user["userId"].(string)
 	token, err := jwtutils.Sign(subject, userToClaims(user), as.jwtSecret)
